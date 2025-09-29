30dc3c3a79dea49c7598f361ec19a659062eda3d
FanixxTC <xxaxelgamerhdxx@gmail.com>
2025-09-29 19:09:51 -0300

    feat: creacion del componente Dashboard real

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom
 import SignUpPage from './pages/SignUpPage'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
-import NavBar from './components/NavBar'
+import Navbar from './components/Navbar'
+import AuthStatus from './components/AuthStatus'
+import Dashboard from './components/Dashboard'
+
 
 const App = () => {
     const userLogin = false
     return (
         <Router>
-            <NavBar/>
+            <AuthStatus/>
+            <Dashboard/>
+            <Navbar/>
             <Routes>
                <Route path="/" element={<HomePage/>}/>
                <Route path="/Signup" element={!userLogin ? <SignUpPage/> : <Navigate to="/"/>}/>
